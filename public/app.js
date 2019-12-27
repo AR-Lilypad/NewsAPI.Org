3ce21b6b31431a1f5a90f99e5b9353c6a09fba51
Angie Roach <angieroach4@gmail.com>
2019-12-26 21:33:22 -0500

    file updates

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
-var cheerio = require("cheerio");
-var axios = require("axios");
+let cheerio = require("cheerio");
+let axios = require("axios");
 
 axios.get("http://www.washingtonpost.com").then(function (res) {
-    var $ = cheerio.load(res.data);
+    let $ = cheerio.load(res.data);
 
-    var headlines = [];
-    var links = [];
-    var blurbs = [];
+    let headlines = [];
+    let links = [];
+    let blurbs = [];
 
     $("h2.headline").each(function (i, element) {
         headlines.push($(element).text());
@@ -26,3 +26,67 @@ axios.get("http://www.washingtonpost.com").then(function (res) {
         console.log(links[i]);
     }
 })
+
+// app.get("/articles", function(req, res) {
+//     db.Article.find({})
+//     .then(function(dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+//   });
+  
+//   app.get("/articles/:id", function(req, res) {
+//     db.Article.findOne({ _id: req.params.id })
+//     .populate("note")
+//     .then(function(dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+//   });
+  
+//   app.post("/saveArticle", function(req, res) {
+//     db.Article.create(req.body)
+//     .then(function(dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+//   });
+  
+//   app.post("/articles/:id", function(req, res) {
+//     db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       return db.Article.findOneAndUpdate({ _id: req.params.id }, {$push: { note: dbNote._id }}, { new: true });
+//     })
+//     .then(function(dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+//   });
+  
+//   app.delete("/notes/:id", function(req, res) {
+//     db.Note.findOneAndDelete({ _id: req.params.id })
+//     .then(function(dbNote) {
+//       res.json(dbNote);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+//   });
+  
+//   app.delete("/articles/:id", function(req, res) {
+//     db.Article.findOneAndDelete({ _id: req.params.id })
+//     .then(function(dbArticle) {
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+//   });
